1348983e83cd4e980d504092dc55dfdfa54fb894
Jessi RC <jessy.ramirez23@gmail.com>
2021-04-03 07:30:54 -0400

    updated config file env variable to fix deployment

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ const db        = {};
 
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(config.use_env_variable);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
