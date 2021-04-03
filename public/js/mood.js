3a8c7876b3812988c9c7e494f252b1a87a4bf3c4
Ivan Orellana Jr <iorellana21@gmail.com>
2021-04-02 20:37:34 -0400

    fixing retake button

--- public/js/mood.js
+++ public/js/mood.js
@@ -128,31 +128,30 @@ video.addEventListener('play', () => {
 //This should be assigned to the results page
 
 
-const bdChanges = () => {
+// const bdChanges = () => {
 
-    const bgContainer = document.getElementById("bg");
+//     const bgContainer = document.getElementById("bg");
 
-    const happyClass = 'happy';
-    const sadClass = 'sad';
-    const neutralClass = 'neutral';
+//     const happyClass = 'happy';
+//     const sadClass = 'sad';
+//     const neutralClass = 'neutral';
 
-    const happyMood = 1;
-    const sadMood = 2;
-    const neutralMood = 3;
+//     const happyMood = 1;
+//     const sadMood = 2;
+//     const neutralMood = 3;
 
-    let mood = happyMood; //mood must be assigned from the facial api functionality above
+//     let mood = happyMood; //mood must be assigned from the facial api functionality above
 
-    if (mood === happyMood) {
-        bgContainer.className = happyClass;
-    }
-    else if (mood === sadMood) {
-        bgContainer.className = sadClass;
-    }
-    else if (mood === neutralMood) {
-        bgContainer.className = neutralClass;
-    }
-};
-
+//     if (mood === happyMood) {
+//         bgContainer.className = happyClass;
+//     }
+//     else if (mood === sadMood) {
+//         bgContainer.className = sadClass;
+//     }
+//     else if (mood === neutralMood) {
+//         bgContainer.className = neutralClass;
+//     }
+// };
 //bdChanges();
 
 // Retake and Continue Buttons
@@ -162,5 +161,5 @@ $("#retake").on("click", function () {
 });
 
 $("#continue").on("click", function () {
-    console.log("onto the music page!");
+    console.log("facethamuzik!");
 });
